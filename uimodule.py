f1421e76bdc05b72343176675a2a35530dd70ed6
Kodie B <kodie.becker@gmail.com>
2020-02-03 00:21:16 -0500

    Adding search
    - Added search. Don't know how well it works yet.

--- uimodule.py
+++ uimodule.py
@@ -1,12 +1,33 @@
 ### Kanka UI Module ###
 
+### TODO:
+### - Add if __name__ = "__main__" functionality
+
 import json
 import time
+import re
+from tabulate import tabulate
 
 def jprint(obj):
     """Create a formatted string of the Python JSON object."""
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
+
+def search():
+    # TODO: Pretty the output, make friendlier to input
+    entity = [('Name','ID','Entity ID')]
+    search_for = input("Search for: ")
+    for d in response.json()['data']:
+        if re.search(response.json()['data'][d]['name'], search_for):
+            name = response.json()['data'][d]['name']
+            id = response.json()['data'][d]['id']
+            entity_id = response.json()['data'][d]['entity_id']
+            entity.append(name, id, entity_id)
+
+    print('Entities Found:\r\n')
+    for a, b, c, in entity:
+        print(a, b, c)
+    print('\r\n')
 
 exit_program = False
 def main():
@@ -16,8 +37,11 @@ def main():
     elif task == "get" or task == "post":
         import apicommunicator
         # Need to figure out how to have global variables...
+    elif task == 'search':
+        search()
     else:
-        print('Invalid input.')
+        print('Invalid input.\r\n')
+
 
 while exit_program == False:
     exit_program = main()
